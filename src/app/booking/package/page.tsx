2d8f947364f6d4484944e80981b4ca3db8c653e8
Omprakash Kumar <omprakash.coder@gmail.com>
2026-06-10 00:26:40 +0530

    Always render RangePackageList, remove inactive feature flag check

--- src/app/booking/package/page.tsx
+++ src/app/booking/package/page.tsx
@@ -1,9 +1,5 @@
-import TheatreList from "@/components/booking/theatre/TheatreList";
 import RangePackageList from "@/components/booking/package/RangePackageList";
 
 export default function PackagePage() {
-  if (process.env.CUSTOMER_RANGE_BOOKING_ENABLED === "true") {
-    return <RangePackageList />;
-  }
-  return <TheatreList />;
+  return <RangePackageList />;
 }
